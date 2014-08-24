1824de4fc522bb61cabb2a23ac9d4b2092e10f74
Roy Riojas <roy@kno.com>
2014-08-24 01:23:04 -0700

    ENH: Added doNotChangeFileExt to prevent changes from .jsx to .js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-module.exports =
-	{ 'preprocessor:react-jsx': ['factory', factory]
-	}
+module.exports = {
+  'preprocessor:react-jsx': ['factory', factory]
+}
 
-var reactTools = require('react-tools')
+var reactTools = require('react-tools');
 
 function factory(args, config, logger, helper) {
-	return function(content, file, done) {
-		if (file.originalPath.substr(-4) == '.jsx') {
-			file.path = file.originalPath.slice(0, -1);
-		}
-		done(reactTools.transform(content))
-	}
+  return function(content, file, done) {
+    // sometimes, it is better to just not change the file extension
+    // if you have a process that do something like this:
+    //
+    // var component = require('./some/component.jsx');
+    //
+    // and you want to test your commonjs module, then it will be better
+    // to not modify the file extension.
+    //
+
+    var cfg = config['react-jsx:preprocessor'] || {};
+    if (!cfg.doNotChangeFileExt && file.originalPath.substr(-4) == '.jsx') {
+      file.path = file.originalPath.slice(0, -1);
+    }
+    done(reactTools.transform(content))
+  }
 }
