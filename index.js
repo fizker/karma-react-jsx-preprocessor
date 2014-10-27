7031618ea111249c78b3f06e7131129411064e8c
Jason Johnston <johnston@nationbuilder.com>
2014-10-27 15:23:18 -0700

    rename factory and add argument injection

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var reactTools = require('react-tools')
 
-function factory(args, config, logger, helper) {
+function createReactJsxPreprocessor(args, config, logger, helper) {
 	return function(content, file, done) {
 		if (file.originalPath.substr(-4) == '.jsx') {
 			file.path = file.originalPath.slice(0, -1);
 		}
-		done(reactTools.transform(content))
+		done(reactTools.transform(content));
 	}
 }
 
+
+createReactJsxPreprocessor.$inject = ['args', 'config.reactJsxPreprocessor', 'logger', 'helper'];
 module.exports = {
-	'preprocessor:react-jsx': ['factory', factory]
-}
+	'preprocessor:react-jsx': ['factory', createReactJsxPreprocessor]
+};
